043e7be2024645ea612db384ba0a9b19f8f692f9
Koen Haagsma <koen.haagsma@themobilecompany.com>
2021-10-25 13:10:45 +0200

    Using only require for loading in data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// const data = require('./tech-track-dataset.json');
+const data = require('./tech-track-dataset.json');
 
-const fs = require('fs');
-const data = fs.readFileSync('./tech-track-dataset.json', 'utf-8');
-const parsedData = JSON.parse(data);
-console.log(parsedData);
+console.log(data);
+
+// const fs = require('fs');
+// const data = fs.readFileSync('./tech-track-dataset.json', 'utf-8');
+// const parsedData = JSON.parse(data);
+// console.log(parsedData);
